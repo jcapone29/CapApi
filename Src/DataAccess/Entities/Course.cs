dd80bdc52d491d4f5ad3561cb0951f24487d0911
Joe Capone <josephcapone529@gmail.com>
2016-03-25 20:19:40 -0400

    Course Class

---
+++ Src/DataAccess/Entities/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Tee { get; set; }
+        public string UsgaRating { get; set; }
+        public string Slope { get; set; }
+
+    }
+}
